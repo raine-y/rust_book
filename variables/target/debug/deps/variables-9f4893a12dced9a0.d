22b8e47b8e751327351fa34abcbf22b5a606f48f
Raine <rainehwalker@proton.me>
2025-10-05 23:05:20 -0400

    blech

---
+++ variables/target/debug/deps/variables-9f4893a12dced9a0.d
@@ -0,0 +1,5 @@
+/Users/raine/Desktop/workspace/rust-book/variables/target/debug/deps/variables-9f4893a12dced9a0.d: src/main.rs
+
+/Users/raine/Desktop/workspace/rust-book/variables/target/debug/deps/libvariables-9f4893a12dced9a0.rmeta: src/main.rs
+
+src/main.rs:
